ccb00d11c0ad5eeb4a265107d2eb737f1015f554
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2022-05-25 20:16:29 +0000

    update

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,12 @@ pub struct Contract {
 
 #[near_bindgen]
 impl Contract {
+    #[init]
+    pub fn new(solution: String) -> Self {
+        Self {
+            crossword_solution: solution,
+        }
+    }
     // ADD CONTRACT METHODS HERE
     pub fn get_puzzle_number(&self) -> u8 {
         PUZZLE_NUMBER
@@ -28,11 +34,16 @@ impl Contract {
     More info in the Nomicon spec.) So while it is not changing the data in the fields of the struct, 
     it does cost some amount of gas to log, requiring a signed transaction by an account that pays for this gas.
     */
-    pub fn guess_solution(&mut self, solution: String) {
-        if solution == self.crossword_solution {
-            env::log_str("You guessed right")
+    pub fn guess_solution(&mut self, solution: String) -> bool {
+        let hashed_input = env::sha256(solution.as_bytes());
+        let hashed_input_hex = hex::encode(&hashed_input);
+
+        if hashed_input_hex == self.crossword_solution {
+            env::log_str("You guessed right");
+            true
         } else {
-            env::log_str("Try again")
+            env::log_str("Try again");
+            false
         }
     }
 }
@@ -57,6 +68,41 @@ mod tests {
         let mut builder = VMContextBuilder::new();
         builder.predecessor_account_id(predecessor);
         builder
+    }
+
+    #[test]
+    fn check_guess_solution() {
+        //get alice as an account id
+        let alice = AccountId::new_unchecked("alice.testnet".to_string());
+        // setup the tessting context and unit test environment
+        let context = get_context(alice);
+        testing_env!(context.build());
+
+        //set up contract object and call the new method
+        let mut contract = Contract::new(
+            "69c2feb084439956193f4c21936025f14a5a5a78979d67ae34762e18a7206a0f".to_string(),
+        );
+        let mut guess_result = contract.guess_solution("wrong answer here".to_string());
+        assert!(!guess_result, "Expected a failure from the wrong guess");
+        assert_eq!(get_logs(), ["Try again"], "Expected a failure log");
+        guess_result = contract.guess_solution("near nomicon ref finance".to_string());
+        assert!(guess_result, "Expected the correct answer to return true.");
+        assert_eq!(
+            get_logs(),
+            ["Try again", "You guessed right"],
+            "Expected a successful log after the previous failed log"
+        );
+    }
+
+    #[test]
+    fn debug_get_hash() {
+        // basic setup for a unit test
+        testing_env!(VMContextBuilder::new().build());
+
+        let debug_solution = "near nomicon ref finance";
+        let debug_hash_bytes = env::sha256(debug_solution.as_bytes());
+        let debug_hash_string = hex::encode(debug_hash_bytes);
+        println!("Let's debug: {:?}", debug_hash_string);
     }
 
     // TESTS HERE
